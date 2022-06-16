9d0a10447ebdd166d0a59751bdd03e28d4452d51
siobhankb <siobhan.boylan@gmail.com>
2022-06-16 16:01:15 -0500

    take a break

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 console.log('yo');
+
+// standingTable
+// ^^ id for div to put table of info
+
+// API address
+// http[s]://ergast.com/api/<series>/<season>/<round>/...
+
+// get season input, add to url
+// get round info, add to url
+// get info from 
+{
+  // get info from input, <form name="season">
+    // handleEvent --> first set function for click
+    const submit = document.querySelectorAll('btn > submit');
+    console.log(submit);
+    let seasonInput = document.getElementsByName('season')
+    let seasonChoice = seasonInput.values
+    console.log(seasonChoice);
+}
